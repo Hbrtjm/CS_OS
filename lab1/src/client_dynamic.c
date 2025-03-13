909feb295137de1dd4222407c72c677308db3182
Hbrtjm <hubi.hey@vp.pl>
2025-03-14 00:21:45 +0100

    Finally fixed it

--- lab1/src/client_dynamic.c
+++ lab1/src/client_dynamic.c
@@ -2,38 +2,37 @@
 #include <stdio.h>
 
 int main() {
-    void *handle = dlopen("libcollatz.so", RTLD_LAZY);
-    if(!handle)
-    {
-    	printf("The library could not be opened");
-	    return 1;
-    }
+    	void *handle = dlopen("libcollatz.so", RTLD_LAZY);
+    	if(!handle)
+    	{
+	    	printf("The library could not be opened");
+		return 1;
+    	}
 
-    int (*test_collatz)(int input, int max_depth, int *steps);
-    test_collatz = (int (*)(int input, int max_depth, int *steps))dlsym(handle,"test_collatz_convergence");
+    	int (*test_collatz)(int input, int max_depth, int *steps);
+    	test_collatz = (int (*)(int input, int max_depth, int *steps))dlsym(handle,"test_collatz_convergence");
 
-    printf("Testing Collatz conjecture using a static library\n");
+    	printf("Testing Collatz conjecture using a static library\n");
 	int available_steps, tested_number;
 	printf("Input the tested numer: ");
 	scanf("%d", &tested_number);
 	printf("Input the amount of steps: ");
 	scanf("%d", &available_steps);
-    int steps[available_steps];
-    int amount_of_steps =     (*test_collatz)(tested_number, available_steps, steps);
+    	int steps[available_steps];
+    	int amount_of_steps =     (*test_collatz)(tested_number, available_steps, steps);
 
-    if (amount_of_steps > 0) {
-        printf("Managed after %d steps:\n", amount_of_steps);
-    } else {
-        printf("Sequence did not converge\n");
-    }
-	if(amount_of_steps == 0)
-	{
-		amount_of_steps = available_steps;
+	if (amount_of_steps > 0) {
+		printf("Managed after %d steps:\n", amount_of_steps);
 	}
-    for (int i = 0; i < amount_of_steps; i++) {
-        printf("%d ", steps[i]);
-    }
-    printf("\n");
-
-    dlclose(handle);
+       	else 
+	{
+		printf("Sequence did not converge\n");
+	    	amount_of_steps = available_steps;
+	}
+	for (int i = 0; i < amount_of_steps; i++) {
+		printf("%d ", steps[i]);
+	}
+	printf("\n");
+	
+	dlclose(handle);
 }
--- lab1/src/client_shared.c
+++ lab1/src/client_shared.c
@@ -3,24 +3,27 @@
 
 int main()
 {
-    printf("Testing Collatz conjecture using a static library\n");
+    	printf("Testing Collatz conjecture using a static library\n");
 	int available_steps, tested_number;
 	printf("Input the tested numer: ");
 	scanf("%d", &tested_number);
 	printf("Input the amount of steps: ");
 	scanf("%d", &available_steps);
-    int steps[available_steps];
-    int amount_of_steps = test_collatz_convergence(tested_number, available_steps, steps);
+    	int steps[available_steps];
+    	int amount_of_steps = test_collatz_convergence(tested_number, available_steps, steps);
 
 
-    if (amount_of_steps > 0) {
-        printf("Managed after %d steps:\n", amount_of_steps);
-    } else {
-        printf("Sequence did not converge\n");
-    }
-    for (int i = 0; i < amount_of_steps; i++) {
-        printf("%d ", steps[i]);
-    }
-    printf("\n");
-    return 0;
+	if (amount_of_steps > 0) {
+		printf("Managed after %d steps:\n", amount_of_steps);
+	} else {
+		
+		amount_of_steps = available_steps;
+		printf("Sequence did not converge\n");
+	}
+	
+	for (int i = 0; i < amount_of_steps; i++) {
+		printf("%d ", steps[i]);
+	}
+	printf("\n");
+	return 0;
 }
