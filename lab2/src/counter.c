b72c72cf32bce1cf58b189471ccdfd8660d61d4a
Hbrtjm <hubi.hey@vp.pl>
2025-03-23 18:14:14 +0100

    Finished lab 2

---
+++ lab2/src/counter.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define RECURSION_LIMIT 1000
+#define PATH_SIZE 512  
+
+int count_size(const char* dirname, int depth)
+{
+	if(depth >= RECURSION_LIMIT)
+	{
+		printf("Recursion depth limit exceeded\n");
+		return 0;
+	}    
+	DIR* mydir = opendir(dirname);
+	if (mydir == NULL)
+	{
+		perror("Error opening directory");
+		return 0;
+	}
+	struct dirent *file;
+	int size = 0;
+	while ((file = readdir(mydir)) != NULL)
+	{
+		if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0)
+			continue;
+		
+		char file_path[PATH_SIZE];
+		snprintf(file_path, sizeof(file_path), "%s/%s", dirname, file->d_name);
+		
+		struct stat file_stat;
+		if (stat(file_path, &file_stat) == -1)
+		{
+			perror("stat error");
+			continue;
+		}
+		
+		if (S_ISDIR(file_stat.st_mode))
+		{
+			size += count_size(file_path, depth + 1);
+		}
+		else
+		{
+			FILE *fp = fopen(file_path, "rb");
+			if (fp)
+			{
+				fseek(fp, 0L, SEEK_END);
+				size += ftell(fp);
+				fclose(fp);
+			}
+			else
+			{
+				perror("Error opening file, check your permissions");
+			}
+		}
+	}
+	closedir(mydir);
+	return size;
+}
+
+int main(int argc, char *argv[])
+{
+	char dirname[PATH_SIZE];
+	
+	if (argc < 2)
+	{
+		printf("Relative path to the directory name: ");
+		scanf("%s", dirname);
+	}
+	else
+	{
+		strncpy(dirname, argv[1], PATH_SIZE - 1);
+		dirname[PATH_SIZE - 1] = '\0';
+	}
+	
+	printf("Counting filesize of %s\n", dirname);
+	printf("The directory takes up: %d bytes\n", count_size(dirname, 1));
+	
+	return 0;
+}
+
--- lab2/src/flipper.c
+++ lab2/src/flipper.c
@@ -21,7 +21,12 @@ int is_text_file(const char *filename) {
 int reverse(char *dirname)
 {
 	DIR* mydir = opendir(dirname);
-        char* newdirname = (char*)malloc(sizeof(char) * (PATH_SIZE + NAME_SIZE));
+        if (mydir == NULL)
+        {
+                perror("Error opening directory");
+                return 1;
+        }
+	char* newdirname = (char*)malloc(sizeof(char) * (PATH_SIZE + NAME_SIZE));
         strcpy(newdirname, dirname);
 	strcat(newdirname, "_reversed");
 	mkdir(newdirname,0777);
@@ -29,60 +34,59 @@ int reverse(char *dirname)
 	int file_size = 0;
 	char content[1000000];
 	int sum = 0;
-	do
+	while((file = readdir(mydir)) != NULL)
         {
 		file_size = 0;
-                file = readdir(mydir);
-                if(file == NULL)
-                {
-                        break;
-                }
-                if(strcmp(file->d_name, ".") != 0 && strcmp(file->d_name, "..") != 0)
+                sum = 0;
+		if(strcmp(file->d_name, ".") != 0 && strcmp(file->d_name, "..") != 0)
 		{
-                char file_path[256];
-                strcpy(file_path, file->d_name);
-        	chdir(dirname);
-                FILE *infile = fopen(file_path,"r");
-                if(infile && is_text_file(file->d_name))
-                {
-                        char *c = (char*)malloc(sizeof(char));
-                        while(fread(c,sizeof(char),1,infile))
-                        {
-                        	content[file_size++] = *c;
-				sum += (int) *c;
-                        }
-                        free(c);
-			char newdir[30];
+                	char file_path[256];
+                	strcpy(file_path, file->d_name);
+        		chdir(dirname);
+                	FILE *infile = fopen(file_path,"r");
+                	
+			if(infile && is_text_file(file->d_name))
+                	{
+                	        char *c = (char*)malloc(sizeof(char));
+                	        while(fread(c,sizeof(char),1,infile))
+                	        {
+                	        	content[file_size++] = *c;
+					sum += (int) *c;
+                	        }
+                	        free(c);
+			
+				chdir("..");
+				chdir(newdirname);
+				char newfile[40];
+				strcpy(newfile, "reversed_");
+				strcat(newfile, file->d_name);
+				FILE *outfile = fopen(newfile,"w");
+				printf("\nWriting into %s...\n",newfile);
+			
+				for(int i = file_size-1;i >= 0;i--)
+				{
+					fwrite((void*)&content[i],sizeof(char),1,outfile);
+				}
+			
+				printf("\nSum of the bytes in the file %s is %d\n", newfile, sum);
+                	}
+                	else
+                	{
+                	        printf("\nProblem with opening the file or the file is a directory: %s\n", file->d_name);
+                	}
 			chdir("..");
-			chdir(newdirname);
-			char newfile[40];
-			strcpy(newfile, "reversed_");
-			strcat(newfile, file->d_name);
-			FILE *outfile = fopen(newfile,"w");
-			printf("\nWriting into %s...\n",newfile);
-			for(int i = file_size-1;i >= 0;i--)
-			{
-				fwrite((void*)&content[i],sizeof(char),1,outfile);
-			}
-			printf("\nSize of the file %s %d\n", newfile, sum);
-                }
-                else
-                {
-                        printf("Problem with opening the file or the file is a directory: %s\n", file->d_name);
-                }
-		chdir("..");
         	}
 	}
-        while(1);
         chdir("..");
 	free(newdirname);		
-	free(dirname);
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
 	char* dirname;
 	dirname = (char*) malloc(sizeof(char) * PATH_SIZE);
+	
 	if(argc < 2)
 	{
 		printf("Relative path to the directory name: ");
@@ -91,8 +95,20 @@ int main(int argc, char *argv[])
 	else
 	{
 		strcpy(dirname,argv[1]);
+		// Makes the program secure, but then it limits the usage to 
+		// directories that have less than 29 characters in the name 
+		dirname[PATH_SIZE - 1] = '\0'; 
 	}
-	printf("%s",dirname);
-	reverse(dirname);
+	
+	printf("Reversing the directory .txt files %s\n", dirname);
+	if(reverse(dirname))
+	{
+		printf("\nAn error has occured while reversing the content of %s\n", dirname);
+	}
+	else
+	{
+		printf("\nThe files in %s have been reversed!\n", dirname);
+	}
+	
 	return 0;
 }
